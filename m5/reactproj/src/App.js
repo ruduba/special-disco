c5d6587b26f98cb2e2f84c44ac85fe0252b6edec
ruduba <vatbudur@gmail.com>
2026-02-12 08:33:40 +0530

    12th Feb, 2026

--- m5/reactproj/src/App.js
+++ m5/reactproj/src/App.js
@@ -3,22 +3,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>THis is my default app, called App! div1</h1>
+
+        <ul>
+          <li> milk</li>
+          <li> bread</li>
+          <li> sugar</li>
+
+        </ul>
+
+        <button>Click me</button>
+
+      </div>
+
+      <div>
+        <h1>THis is my default app, called App! div2</h1>
+
+        <ul>
+          <li> milk</li>
+          <li> bread</li>
+          <li> sugar</li>
+
+        </ul>
+
+        <button>Click me</button>
+
+      </div>
+
+    </>
   );
 }
 
