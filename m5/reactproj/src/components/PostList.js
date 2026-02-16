7ffeb40f74f991fd3513bb0509dee05076ca95a8
ruduba <vatbudur@gmail.com>
2026-02-16 08:00:17 +0530

    16th Feb, 2026

--- m5/reactproj/src/components/PostList.js
+++ m5/reactproj/src/components/PostList.js
@@ -1,25 +1,69 @@
+import { useEffect, useState } from "react";
 
-function PostList(){
+function PostList() {
+    const url = "https://jsonplaceholder.typicode.com/posts"
 
-return(
-    <>
+    useEffect(() => {
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network issue!!')
+                } else {
+                    console.log("data fetch successful!")
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setPosts(data);
+            })
+            .catch((err) => {
+                console.log("oops there is some issue")
+            })
+    }, []) // runs only once
 
-    <h1>HI GIYS</h1>
-    </>
+    let [posts, setPosts] = useState([]);
+
+    function handleDelete(delItem){
+        //alert('do you really want to delete the post?')
+        posts = [...posts.filter((delId)=> delId.id !== delItem)]
+        setPosts(posts);
+    }
+
+    return (
+        <>
+            <div className="container">
+                <h1 className="display-1">Posts</h1>
+                <table>
+                    <thead>
+
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Body</th>
+                        <th>Actions</th>
+
+                    </thead>
+                    <tbody>
+                        {posts.map((post) => (
+                            <tr key={post.id}>
+                                <td>{post.id}</td>
+                                <td>{post.title}</td>
+                                <td>{post.body}</td>
+                                <td><button className="btn btn-danger" onClick={()=>handleDelete(post.id)}>delete</button>
+                                </td>
+                            </tr>
+                        )
+                        )}
+
+                    </tbody>
 
 
+                </table>
+
+            </div>
 
 
-
-
-
-
-)
-
-
-
-
-
+        </>
+    )
 }
 
 export default PostList;
