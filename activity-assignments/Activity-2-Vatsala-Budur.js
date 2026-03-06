34f916ee611e200cfe92472ad3306151ffd78b82
ruduba <vatbudur@gmail.com>
2026-03-06 22:58:18 +0530

    Activity 2

---
+++ activity-assignments/Activity-2-Vatsala-Budur.js
@@ -0,0 +1,24 @@
+// creating an object using object literal
+const student = {
+  name: "Kiran",
+  age: 20,
+  course: "Computer Science",
+  university: "Edutainer University",
+  isEnrolled: true
+};
+
+// accessing object properties
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Course:", student.course);
+console.log("University:", student.university);
+console.log("Enrollment Status:", student.isEnrolled);
+
+/*A JavaScript object is a collection of key–value pairs used to store related data in a structured way. 
+In the example above, the object student represents information about a student. 
+Each property has a key (such as name, age, course) and an associated value (like "Rahul" or 20).
+Objects are created using object literals with curly braces {}. 
+Properties inside the object are separated by commas. 
+We can access the values of properties using dot notation (student.name) 
+or bracket notation (student["name"]). Objects help organize data and make programs easier 
+to manage when dealing with related information.*/
